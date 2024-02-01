6517eedd81f34d41e950b8f191149aaa5b8916b1
Andria Bakti Mahendra <andriabaktimahendra@gmail.com>
2024-02-02 04:43:52 +0700

    setup(auth-middleware): setting up middleware for authentication

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,15 +1,17 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthMiddleware } from './commons/middlewares/auth.middleware';
 import {
   BaseConfigModule,
   ThrottlerProvider,
 } from './configs/base-config.module';
 import { DatabaseModule } from './configs/database/database.module';
 import { RedisModule } from './configs/redis/redis.module';
+import { LinkController } from './modules/links/link.controller';
+import { LinkModule } from './modules/links/link.module';
 import { LinkService } from './modules/links/link.service';
 import { UserModule } from './modules/users/user.module';
-import { LinkModule } from './modules/links/link.module';
 
 @Module({
   imports: [
@@ -22,4 +24,8 @@ import { LinkModule } from './modules/links/link.module';
   controllers: [AppController],
   providers: [ThrottlerProvider, AppService, LinkService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes(LinkController);
+  }
+}
---
+++ apps/server/src/commons/middlewares/auth.middleware.ts
@@ -0,0 +1,49 @@
+import {
+  BadRequestException,
+  Injectable,
+  NestMiddleware,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from '@server/modules/users/user.service';
+import { NextFunction, Request, Response } from 'express';
+
+@Injectable()
+export class AuthMiddleware implements NestMiddleware {
+  constructor(
+    private readonly jwtSvc: JwtService,
+    private readonly userSvc: UserService,
+  ) {}
+
+  async use(req: Request, res: Response, next: NextFunction) {
+    const reqAuth: string = req.headers?.authorization;
+    if (!reqAuth) {
+      throw new BadRequestException('Token is Required!');
+    } else {
+      const tokenArr = reqAuth.split(' ');
+      if (tokenArr[0] != 'Bearer' || tokenArr.length < 2) {
+        throw new UnauthorizedException('Token is Not Valid!');
+      }
+
+      let decoded: { email: string; iat: number; exp: number };
+      try {
+        decoded = await this.jwtSvc.verifyAsync(tokenArr[1]);
+      } catch (err) {
+        const errMsg =
+          err.message == 'TokenExpiredError'
+            ? 'Token is Expired!'
+            : err.message;
+        throw new UnauthorizedException(errMsg);
+      }
+
+      const existUser: { id: string } = await this.userSvc.findByEmail(
+        decoded.email,
+      );
+      if (!existUser) throw new NotFoundException('User is Not Exist!');
+      req['user'] = existUser;
+
+      next();
+    }
+  }
+}
